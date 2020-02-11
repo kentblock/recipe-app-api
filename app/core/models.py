9f5b3de239c9008499b018aacdd66750a1368064
Riley Peel <rpeel4@outlook.com>
2020-02-11 10:22:35 -0800

    Updated admin to support custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -16,7 +16,7 @@ class UserManager(BaseUserManager):
 
 	def create_superuser(self, email, password):
 		"""Creates and saves new superuser"""
-		user = self.create_user
+		user = self.create_user(email, password)
 		user.is_staff=True
 		user.is_superuser=True
 		user.save(using = self._db)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -12,4 +12,34 @@ class AdminSiteTests(TestCase):
 			email = 'admin@googley.com',
 			password = 'password123'
 		)
-	self.client.force_login(self.admin_user)
+		self.client.force_login(self.admin_user)
+		self.user = get_user_model().objects.create_user(
+			email = 'test@googley.com',
+			password = 'password1223',
+			name='Text user full name'
+		)
+
+	def test_users_listed(self):
+		"""Test that users are lised on user page"""
+		url = reverse('admin:core_user_changelist')
+		res = self.client.get(url)
+
+		self.assertContains(res, self.user.name)
+		self.assertContains(res, self.user.email)
+
+
+	def test_user_change_page(self):
+		url = reverse('admin:core_user_change', args=[self.user.id])
+		# /admin/core/user/
+		res = self.client.get(url)
+		self.assertEqual(res.status_code, 200)
+
+
+	def test_create_user_page(self):
+		"""Test create user page renders correctly"""
+		url = reverse('admin:core_user_add')
+		res = self.client.get(url)
+		self.assertEqual(res.status_code, 200)
+
+
+
